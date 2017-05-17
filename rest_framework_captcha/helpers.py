07a2a7851343bf0cb1cd7d15f21bb4b4217c9e86
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-05-17 16:27:47 -0300

    Create helpers

--- rest_framework_captcha/helpers.py
+++ rest_framework_captcha/helpers.py
@@ -1,6 +1,17 @@
 from django.conf import settings
 from django.utils.translation import ugettext as _
-
+import importlib
 
 def get_settings(string="REST_FRAMEWORK_CAPTCHA"):
   return getattr(settings, string, {})
+
+def import_from_string(val):
+  try:
+    # Nod to tastypie's use of importlib.
+    parts = val.split('.')
+    module_path, class_name = '.'.join(parts[:-1]), parts[-1]
+    module = importlib.import_module(module_path)
+    return getattr(module, class_name)
+  except ImportError as e:
+    msg = "Could not import '%s' for setting. %s: %s." % (val, e.__class__.__name__, e)
+    raise ImportError(msg)
